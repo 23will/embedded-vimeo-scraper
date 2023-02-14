ba000bf344820f0aaf0b7d61b56a7ab5225ba82b
Your Full Name <your@email.tld>
2023-02-14 16:11:49 +0000

    Adding intermediate CSV writing and reading

--- run.py
+++ run.py
@@ -1,77 +1,31 @@
-import requests
-from bs4 import BeautifulSoup
 import pycookiecheat
-from vimeo_downloader import Vimeo
-import os
-from urllib.parse import urljoin
-from dataclasses import dataclass
+from video_url_grabber import VideoCsvWriter, extract_root_url
+from video_downloader import VideoDownloader
 
 
-@dataclass
-class Video:
-    video_url: str
-    embedded_url: str
-    parent_url: str
-    title: str
-    def get_filename(self):
-        return self.parent_url.replace('/', '_') + '_' + self.title
+#TODO logging, cli, linting, settings file
+
+CSV_DIR = 'csv'
+DOWNLOAD_DIR = 'videos'
+HTML_CACHE_DIR = 'html_cache'
+CSV_FILENAME = 'videos.csv'
+MISSING_URLS_CSV_FILENAME = 'missing_urls.csv'
+URL = 'https://someurl.com/stuff/'
 
 
-root_url = ''
-visited_urls = []
-videos = []
+root_url = URL.split('.com')[0] + '.com'
 
-
-def get_html(url, cookies):
-    response = requests.get(url, cookies=cookies)
-    return response.text if response.status_code == 200 else None
-
-
-def find_links(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    a_tags = soup.find_all('a')
-    hrefs = dict()
-    for a_tag in a_tags:
-        href = a_tag.get('href')
-        if href is not None and href.startswith(root_url) and a_tag.string is not None:
-            href = a_tag['href']
-            hrefs[href] = a_tag.string
-    return hrefs
-
-
-def find_videos(html, url, previous_url_suffix):
-    inner_videos = []
-    soup = BeautifulSoup(html, 'html.parser')
-    a_tags = soup.find_all('iframe')
-    for a_tag in a_tags:
-        h1_tag = a_tag.find_previous('h1')
-        src = a_tag.get('src')
-        inner_videos.append(Video(src, url, previous_url_suffix, h1_tag.string))
-    return inner_videos
-
-
-def populate_videos(url_suffix, previous_url_suffix, cookies):
-    url = urljoin(root_url, url_suffix)
-    if url not in visited_urls:
-        print(f'URL: {url}')
-        visited_urls.append(url)
-        html = get_html(url, cookies)
-        if html is not None:
-            links = find_links(html)
-            inner_videos = find_videos(html, url, previous_url_suffix)
-            videos.extend(inner_videos)
-            for link, title in links.items():
-                populate_videos(link.lstrip(root_url), url_suffix, cookies)
-
-
-def download_videos(cookies):
-    for v in videos:
-        file_name = v.get_filename()
-        print(file_name)
-        v = Vimeo(v.video_url, v.embedded_url)
-        v.streams[0].download(download_directory='videos', filename=file_name)
-
-
+#requires you to have logged in on chrome as it all works with cookies
 cookies = pycookiecheat.chrome_cookies(root_url)
-# populate_videos('/', None, cookies)
-download_videos(cookies)
+
+
+#Recursively looks through html for url links and embedded video links and writes to a CSV in /csv path, caches the html for performance in /html_cache path
+#This CSV contains urls that don't contain any video links as a sanity check
+video_grabber = VideoCsvWriter(CSV_DIR, HTML_CACHE_DIR, cookies, root_url, URL, CSV_FILENAME, MISSING_URLS_CSV_FILENAME)
+video_grabber.write_videos_to_csv()
+
+video_downloader = VideoDownloader(CSV_DIR, DOWNLOAD_DIR, CSV_FILENAME, root_url)
+#dryrun returns list of filenames that can be downloaded given the videos parent url contains someefilter
+video_downloader.dryrun('somefilter')
+#video_downloader.download_videos('sound')
+
---
+++ video_url_grabber.py
@@ -0,0 +1,130 @@
+import csv 
+import os
+import requests
+
+from video import Video
+from bs4 import BeautifulSoup
+
+
+def extract_root_url(url: str) -> str:
+    return url.split('.com')[0] + '.com'
+
+
+class VideoCsvWriter:
+
+
+    def __init__(self, csv_dir, html_cache_dir, cookies, url: str, filename, missing_urls_filename) -> None:
+        self.__csv_dir = csv_dir
+        self.__html_cache_dir = html_cache_dir
+        self.__cookies = cookies
+        self.__url = url
+        self.__root_url = extract_root_url(url)
+        self.__visited_urls = []
+        self.__videos = []
+        self.__filename = filename
+        self.__missing_urls_filename = missing_urls_filename
+
+
+    def __create_path_if_not_exists(self, dir):
+        output_path = os.path.join(os.getcwd(), dir)
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
+        return output_path
+
+
+    def __get_html_cache_path(self, url):
+        suffix = url.replace(self.__root_url, '')
+        print(f'Stripping {self.__root_url} from {url} = {suffix}')
+        html_filename = suffix.replace('/','_') + '.html'
+        output_path = self.__create_path_if_not_exists(self.__html_cache_dir)
+        return os.path.join(output_path, html_filename)
+
+
+    def __get_html(self, url):
+        html_cache_path = self.__get_html_cache_path(url)
+        if os.path.exists(html_cache_path):
+            with open(html_cache_path, 'r') as f:
+                print ('Reading html from ' + html_cache_path)
+                return f.read()
+        else:
+            response = requests.get(url, cookies=self.__cookies)
+            html = response.text if response.status_code == 200 else None
+            if html is not None:
+                with open(html_cache_path, 'w') as f:
+                    f.write(html)
+            return html
+
+
+    def __safe_decompose(self, tag):    
+        if tag is not None:
+            tag.decompose()
+
+
+    def __find_links(self, html):
+        soup = BeautifulSoup(html, 'html.parser')
+        #remove header and footer as they contain unnecessary links
+        self.__safe_decompose(soup.footer)
+        self.__safe_decompose(soup.header)
+
+        a_tags = soup.find_all('a')
+        hrefs = dict()
+        for a_tag in a_tags:
+            href = a_tag.get('href')
+            if href is not None and href.startswith(self.__root_url) and href not in self.__visited_urls and a_tag.string is not None:
+                href = a_tag['href']
+                hrefs[href] = a_tag.string
+        return hrefs
+
+
+    def __find_videos(self, html, csv_writer, url, previous_url, title):
+        inner_videos = []
+        soup = BeautifulSoup(html, 'html.parser')
+        a_tags = soup.find_all('iframe')
+        for a_tag in a_tags:
+            h1_tag = a_tag.find_previous('h1')
+            src = a_tag.get('src')
+            video = Video(h1_tag.string, url, src, title, previous_url)
+            csv_writer.writerow(video)
+            self.__stream.flush()
+            print(f'Found video: ${video.title}')
+            inner_videos.append(video)
+        return inner_videos
+
+
+    def __populate_videos(self, url, csv_writer, previous_url=None, title=None):
+        if url not in self.__visited_urls:
+            print(f'URL: {url}')
+            self.__visited_urls.append(url)
+            html = self.__get_html(url)
+            if html is not None:
+                inner_videos = self.__find_videos(html, csv_writer, url, previous_url, title)
+                self.__videos.extend(inner_videos)
+                links = self.__find_links(html)
+                for link, title in links.items():
+                    self.__populate_videos(link, csv_writer, url, title)
+
+
+    def __get_urls_without_videos(self):
+        video_urls = list(map(lambda v: v.url, self.__videos))        
+        video_parent_urls = list(map(lambda v: v.parent_url, self.__videos))        
+        return list(filter(lambda u: u not in video_urls and u not in video_parent_urls, self.__visited_urls))
+
+
+    def ___write_missing_video_urls_csv(self):
+        urls = self.__get_urls_without_videos()
+        csv_dir = self.__create_path_if_not_exists(self.__csv_dir)
+        csv_path = os.path.join(csv_dir, self.__missing_urls_filename)
+        with open(csv_path, "w") as stream:
+            writer = csv.writer(stream)
+            for url in urls:
+                writer.writerow([url])
+    
+
+    def write_videos_to_csv(self):
+        csv_dir = self.__create_path_if_not_exists(self.__csv_dir)
+        csv_path = os.path.join(csv_dir, self.__filename)
+        with open(csv_path, "w") as stream:
+            self.__stream = stream
+            csv_writer = csv.writer(stream)
+            self.__populate_videos(self.__url, csv_writer)
+        self.___write_missing_video_urls_csv()
